f245ec51058b33d4472f4dffcc88fe2427f3794d
dermut <ultimate1994@naver.com>
2024-09-21 16:41:19 +0900

    style: private 알림 제거

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,10 @@ class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  LoginPageState createState() => LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class LoginPageState extends State<LoginPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
